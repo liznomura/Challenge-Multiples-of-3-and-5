d609e56152bfa1a058a914716de14c65c7d5c0ad
liznomura <lizn@hawaii.edu>
2017-06-13 10:15:20 -1000

    deleted extra line, cleaned up extra spaces

--- multiples.js
+++ multiples.js
@@ -6,10 +6,9 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
   for (var i = 0; i < n; i++) {
-    if ( i % 3 === 0 || i % 5 === 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
-
   }
   return sum;
 };
